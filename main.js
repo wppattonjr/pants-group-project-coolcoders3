769574273d4bf63fa08ab84c3f4efa823d86f746
Kaitlyn VanHook <kaitlynvanhook@Kaitlyns-MacBook-Pro-2.local>
2020-08-10 19:39:06 -0500

    added images folder and array of jeans objects

--- main.js
+++ main.js
@@ -1,3 +1,48 @@
 'use strict';
 
-console.log('hey javascript');
+const allJeans = [
+    {
+        name: 'Mid Rise Skinny Jeans',
+        style: 'Skinny',
+        price: 49.95,
+        imageUrl: 'images/midRiseSkinny',
+        sizesAvailable: ['24', '25', '26', '27', '28', '29', '30', '31', '32', '33', '34', '35']
+    }, 
+    {
+        name: 'High Rise Skinny Jeans',
+        style: 'Skinny',
+        price: 79.95,
+        imageUrl: 'images/highRiseSkinny',
+        sizesAvailable: ['24', '25', '26', '27', '28', '29', '30', '31', '32', '33', '34', '35']
+    }, 
+    {
+        name: 'Mid Rise Distressed Jeans',
+        style: 'Distressed',
+        price: 89.95,
+        imageUrl: 'images/midRiseDistressed',
+        sizesAvailable: ['24', '25', '26', '27', '28', '29', '30', '31', '32', '33', '34', '35']
+    }, 
+    {
+        name: 'High Rise Distressed Jeans',
+        style: 'Distressed',
+        price: 79.95,
+        imageUrl: 'images/highRiseDistressed',
+        sizesAvailable: ['24', '25', '26', '27', '28', '29', '30', '31', '32', '33', '34', '35']
+    }, 
+    {
+        name: 'Mid Rise Flare Jeans',
+        style: 'Flare',
+        price: 89.95,
+        imageUrl: 'images/midRiseFlare',
+        sizesAvailable: ['24', '25', '26', '27', '28', '29', '30', '31', '32', '33', '34', '35']
+    }, 
+    {
+        name: 'High Rise Flare Jeans',
+        style: 'Flare',
+        price: 109.95,
+        imageUrl: 'images/highRiseFlare',
+        sizesAvailable: ['24', '25', '26', '27', '28', '29', '30', '31', '32', '33', '34', '35']
+    }, 
+]
+
+
