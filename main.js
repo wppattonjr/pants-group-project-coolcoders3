379f5ae858eecf613b655958be2b48ef74b8ab83
Sam Mudick <smudick@gmail.com>
2020-08-10 20:50:18 -0500

    added array of fun facts objects

--- main.js
+++ main.js
@@ -1,3 +1,72 @@
 'use strict';
 
 console.log('hey javascript');
+
+
+let funFacts = [
+    {
+        title : 'Fun Fact #001',
+        content : 'Jeans are cool',
+        id: 1
+    },
+    {
+        title : 'Fun Fact #002',
+        content : 'Jeans are very cool',
+        id: 2
+    },
+    {
+        title : 'Fun Fact #003',
+        content : 'Jeans are super cool',
+        id: 3
+    },
+    {
+        title : 'Fun Fact #004',
+        content : 'Jeans are really cool',
+        id: 4
+    },
+    {
+        title : 'Fun Fact #005',
+        content : 'Jeans are extra cool',
+        id: 5
+    },
+    {
+        title : 'Fun Fact #006',
+        content : 'Jeans are my favorite type of pants',
+        id: 6
+    },
+    {
+        title : 'Fun Fact #007',
+        content : 'Buy our jeans',
+        id: 7
+    },
+    {
+        title : 'Fun Fact #008',
+        content : 'Jeans are like a hat for your legs',
+        id: 8
+    },
+    {
+        title : 'Fun Fact #009',
+        content : 'Jeans are sometimes blue',
+        id: 9
+    },
+    {
+        title : 'Fun Fact #010',
+        content : 'Jeans are sometimes not blue',
+        id: 10
+    },
+    {
+        title : 'Fun Fact #011',
+        content : 'Jeans were invented a long time ago',
+        id: 11
+    },
+    {
+        title : 'Fun Fact #012',
+        content : 'Jeans are made of denim',
+        id: 12
+    },
+    {
+        title : 'Fun Fact #013',
+        content : 'Jeans are kind of irrelevant due to quarantine',
+        id: 13
+    },
+]
