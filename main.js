6344f2d8ebdaeaf3228febe00cb30bae2849e062
Kaitlyn VanHook <kaitlynvanhook@Kaitlyns-MacBook-Pro-2.local>
2020-08-10 20:15:33 -0500

    card added on html page

--- main.js
+++ main.js
@@ -5,44 +5,55 @@ const allJeans = [
         name: 'Mid Rise Skinny Jeans',
         style: 'Skinny',
         price: 49.95,
-        imageUrl: 'images/midRiseSkinny',
+        imageUrl: 'images/midRiseSkinny.png',
         sizesAvailable: ['24', '25', '26', '27', '28', '29', '30', '31', '32', '33', '34', '35']
     }, 
     {
         name: 'High Rise Skinny Jeans',
         style: 'Skinny',
         price: 79.95,
-        imageUrl: 'images/highRiseSkinny',
+        imageUrl: 'images/highRiseSkinny.png',
         sizesAvailable: ['24', '25', '26', '27', '28', '29', '30', '31', '32', '33', '34', '35']
     }, 
     {
         name: 'Mid Rise Distressed Jeans',
         style: 'Distressed',
         price: 89.95,
-        imageUrl: 'images/midRiseDistressed',
+        imageUrl: 'images/midRiseDistressed.png',
         sizesAvailable: ['24', '25', '26', '27', '28', '29', '30', '31', '32', '33', '34', '35']
     }, 
     {
         name: 'High Rise Distressed Jeans',
         style: 'Distressed',
         price: 79.95,
-        imageUrl: 'images/highRiseDistressed',
+        imageUrl: 'images/highRiseDistressed.png',
         sizesAvailable: ['24', '25', '26', '27', '28', '29', '30', '31', '32', '33', '34', '35']
     }, 
     {
         name: 'Mid Rise Flare Jeans',
         style: 'Flare',
         price: 89.95,
-        imageUrl: 'images/midRiseFlare',
+        imageUrl: 'images/midRiseFlare.png',
         sizesAvailable: ['24', '25', '26', '27', '28', '29', '30', '31', '32', '33', '34', '35']
     }, 
     {
         name: 'High Rise Flare Jeans',
         style: 'Flare',
         price: 109.95,
-        imageUrl: 'images/highRiseFlare',
+        imageUrl: 'images/highRiseFlare.png',
         sizesAvailable: ['24', '25', '26', '27', '28', '29', '30', '31', '32', '33', '34', '35']
     }, 
 ]
 
+const printToDom = (divId, textToPrint) => {
+    const selectedDiv = document.getElementById(divId);
+    selectedDiv.innerHTML = textToPrint;
+}
 
+const buildProductCards = (arr) => {
+    let domString = '';
+
+    for (let i = 0; i < arr.length; i++) {
+        domString +=
+    }
+}
