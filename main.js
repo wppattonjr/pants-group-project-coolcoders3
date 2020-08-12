04b21997c6289ca252f68bdb3c03c4c7dd959e0f
Kaitlyn VanHook <kaitlynvanhook@Kaitlyns-MacBook-Pro-2.local>
2020-08-11 19:59:54 -0500

    working on filter by price and filter by style

--- main.js
+++ main.js
@@ -58,8 +58,9 @@ const buildProductCards = (arr) => {
                         <img src="${allJeans[i].imageUrl}" class="card-img-top" alt="${allJeans[i].name}">
                         <div class="card-body">
                             <h5 class="product-name">${allJeans[i].name}</h5>
+                            <p class="product-price">$${allJeans[i].price}</p>
                             <p class="available-sizes">Available Sizes</p>
-                            <p class="card-sizes">${allJeans[i].sizesAvailable.join('  ')}</p>
+                            <p class="card-sizes">${allJeans[i].sizesAvailable.join(' ')}</p>
                         </div>
                     </div>`
     }
@@ -67,8 +68,60 @@ const buildProductCards = (arr) => {
     printToDom('productContainerShop', domString);
 }
 
+const buttonEvents = () => {
+    document.getElementById('styleFilterDropdown').addEventListener('click', filterByStyle);
+    document.getElementById('priceFilterDropdown').addEventListener('click', filterByPrice);
+}
+
+const filterByStyle = (e) => {
+    console.log(e.target.id)
+    const buttonId = e.target.id;
+    const ctype = e.target.type;
+
+    const selectedStyles = [];
+
+    for (let i =0; i < allJeans.length; i++) {
+        switch (buttonId) {
+            case 'Skinny':
+                selectedStyles.push(allJeans[i]);
+                buildProductCards(selectedStyles);
+                break;
+            case 'Distressed':
+                selectedStyles.push(allJeans[i]);
+                buildProductCards(selectedStyles);
+                break;
+            case 'Flare':
+                selectedStyles.push(allJeans[i]);
+                buildProductCards(selectedStyles);
+                break;
+            case 'allStylesSort':
+                buildProductCards(allJeans);
+                break;
+        }
+    }
+
+    // for (let i = 0; i < allJeans.length; i++) {
+    //     if (allJeans[i].style === buttonId) {
+    //         selectedStyles.push(allJeans[i]);
+    //     }
+    // }
+
+    // console.log(selectedStyles)
+
+    // if (buttonId === 'allStylesSort' && ctype === 'button') {
+    //     buildProductCards(allJeans);
+    // } else if (ctype === 'button') {
+    //     buildProductCards(selectedStyles);
+    // }
+}
+
+const filterByPrice = (e) => {
+    
+}
+
 const init = () => {
     buildProductCards(allJeans);
+    buttonEvents();
 }
 
 init();
