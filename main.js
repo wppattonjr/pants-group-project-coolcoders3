a28436378eea566511513af553bb3984a2951ef5
Sam Mudick <smudick@gmail.com>
2020-08-13 22:34:01 -0500

    Added map colors and click functionality to show location cards

--- main.js
+++ main.js
@@ -192,42 +192,54 @@ const allJeans = [
 ];
 
 const locations = [
-    {
-        imageUrl: '/images/nashville',
-        city: 'Nashville',
-        address: '1234 5th Street Nashville, TN',
-        hours: {
-            weekdays: '10am - 7pm',
-            weekends: '11am - 5pm'
-        },
-        id: 'TN'
+  {
+    imageUrl: "images/nashville.jpeg",
+    city: "Nashville",
+    address: "1234 5th Street Nashville, TN",
+    hours: {
+      weekdays: "10am - 7pm",
+      weekends: "11am - 5pm",
     },
-    {
-        imageUrl: '/images/chicago',
-        city: 'Chicago',
-        address: '312 Main Street Chicago, IL',
-        id: 'IL'
+    id: "TN",
+    color: "#EA526F",
+    delay: ".5s",
+  },
+  {
+    imageUrl: "images/chicago.jpg",
+    city: "Chicago",
+    address: "312 Main Street Chicago, IL",
+    hours: {
+      weekdays: "TBD",
+      weekends: "TBD",
     },
-    {
-        imageUrl: '/images/NY',
-        city: 'New York',
-        address: '480 Madison Avenue NY, NY',
-        hours: {
-            weekdays: '10am - 9pm',
-            weekends: '12pm - 6pm'
-        },
-        id: 'NY'
+    id: "IL",
+    color: "#FF8A5B",
+    delay: "2s",
+  },
+  {
+    imageUrl: "images/NY.jpg",
+    city: "New York",
+    address: "480 Madison Avenue NY, NY",
+    hours: {
+      weekdays: "10am - 9pm",
+      weekends: "12pm - 6pm",
     },
-    {
-        imageUrl: '/images/LA',
-        city: 'Los Angeles',
-        address: '9876 Sunset Blvd Los Angeles, CA',
-        hours: {
-            weekdays: '9am - 6pm',
-            weekends: '10pm - 4pm'
-        },
-        id: 'CA'
-    }          
+    id: "NY",
+    color: "#25CED1",
+    delay: "1s",
+  },
+  {
+    imageUrl: "images/LA.jpg",
+    city: "Los Angeles",
+    address: "9876 Sunset Blvd Los Angeles, CA",
+    hours: {
+      weekdays: "9am - 6pm",
+      weekends: "10pm - 4pm",
+    },
+    id: "CA",
+    color: "#FCEADE",
+    delay: "1.5s",
+  },
 ];
 
 const printToDom = (divId, textToPrint) => {
@@ -235,6 +247,7 @@ const printToDom = (divId, textToPrint) => {
   selectedDiv.innerHTML = textToPrint;
 };
 
+//////////////////// SHOP PAGE FUNCTIONS//////////////////////////
 const buildProductCards = (arr) => {
   let domString = "";
 
@@ -256,21 +269,68 @@ const buildProductCards = (arr) => {
   printToDom("productContainerShop", domString);
 };
 
+
+//////////////////// ABOUT PAGE FUNCTIONS//////////////////////////
+const colorMap = () => {
+  for (let i = 0; i < locations.length; i++) {
+    document.querySelector(`.${locations[i].id}`).style.fill =
+      locations[i].color;
+    document.querySelector(`.${locations[i].id}`).style.transitionDelay =
+      locations[i].delay;
+  }
+};
+
 const mapHover = (state) => {
-    document.querySelector(state).addEventListener('hover', displayLocationCard);
-    document.querySelector(state).addEventListener('hover', colorState)
-}
+  document
+    .querySelector(state)
+    .addEventListener("mouseover", highlightLocation);
+};
+const mapOffHover = (state) => {
+  document
+    .querySelector(state)
+    .addEventListener("mouseout", unhiglightLocation);
+};
+const highlightLocation = (e) => {
+  let locationId = e.target.id;
+  for (let i = 0; i < locations.length; i++) {
+    if (locationId === locations[i].id) {
+      document.querySelector(`.${locations[i].id}`).style.stroke = "black";
+      document.querySelector(`.${locations[i].id}`).style.transitionDelay =
+        ".2s";
+    }
+  }
+};
+const unhiglightLocation = (e) => {
+  let locationId = e.target.id;
+  for (let i = 0; i < locations.length; i++) {
+    if (locationId === locations[i].id) {
+      document.querySelector(`.${locations[i].id}`).style.stroke = "none";
+    }
+  }
+};
+const mapClick = (state) => {
+  document.querySelector(state).addEventListener("click", displayLocationCard);
+};
 
 const displayLocationCard = (e) => {
-    const locationId = e.target.id;
-    domString = ''
-    for (i = 0; i < locations.length ; i++) {
-        if (locationId === locations[i].id) {
-
-        }
+  let locationId = e.target.id;
+  let domString = "";
+  for (let i = 0; i < locations.length; i++) {
+    if (locationId === locations[i].id) {
+      domString += `<div class="card text-center" style="width: 18rem;">
+<img class="card-img-top" src="${locations[i].imageUrl}" alt="${locations[i].city} Storefront">
+<div class="card-body">
+  <h3 class="card-text">${locations[i].city}</h3>
+  <p class="card-text">${locations[i].address}</p>
+  <h5 class="card-text">Hours</h5>
+  <p class="card-text">Weekdays: ${locations[i].hours.weekdays}</p>
+  <p class="card-text">Weekends: ${locations[i].hours.weekends}</p>
+</div>
+</div>`;
     }
-}
-
+  }
+  printToDom("locationCardAbout", domString);
+};
 
 const funFactsButtonClick = () => {
   document
@@ -294,9 +354,23 @@ const displayRandomFact = () => {
   printToDom("factsCardAbout", domString);
 };
 
+///////////////// GLOBAL FUNCTIONs ////////////////////////
 const init = () => {
   switch (document.location.pathname) {
     case "/about.html":
+      colorMap();
+      mapHover("#TN");
+      mapHover("#NY");
+      mapHover("#CA");
+      mapHover("#IL");
+      mapOffHover("#TN");
+      mapOffHover("#NY");
+      mapOffHover("#CA");
+      mapOffHover("#IL");
+      mapClick("#TN");
+      mapClick("#NY");
+      mapClick("#CA");
+      mapClick("#IL");
       funFactsButtonClick();
       break;
     case "/shop.html":
